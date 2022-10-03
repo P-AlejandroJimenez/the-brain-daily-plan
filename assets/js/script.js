ebb53af23bb5da8ddf99018f7a8e07797733ea43
P-AlejandroJimenez <110042398+P-AlejandroJimenez@users.noreply.github.com>
2022-10-03 07:37:09 -0600

    The js file was added and linked to the index.html file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,7 @@
-//var timeblockEl = $(#time-block);
-//<p id="currentDay" class="lead"></p>
-//var currentDayEl = $(moment().format('llll');)
-var currentDayEl = $('#current-day');
+var currentDayEl = $('#currentDay')
 
-
-function displayTime() {
+function displayTime () {
     var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     currentDayEl.text(currentTime);
 }
+setInterval(displayTime, 1000);
