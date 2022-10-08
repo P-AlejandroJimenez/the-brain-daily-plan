629cf55b934929a3103a4990a4244d5ced19f8fc
P-AlejandroJimenez <110042398+P-AlejandroJimenez@users.noreply.github.com>
2022-10-08 10:55:45 -0600

    I had to review and start from scratch. I believe I know understand how to iplement bootstrap.

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var currentDayEl = $('#currentDay')
-
-function displayTime () {
-    var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    currentDayEl.text(currentTime);
-}
-setInterval(displayTime, 1000);
