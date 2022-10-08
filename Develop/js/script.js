3102ba14a8acfd32c218c31c07bb6d26bdbdd81e
P-AlejandroJimenez <110042398+P-AlejandroJimenez@users.noreply.github.com>
2022-10-08 12:16:42 -0600

    Styling through bootstrap was completed. A javaScript file was added app functionality was started.

---
+++ Develop/js/script.js
@@ -0,0 +1,9 @@
+var currentDayEl = $('#currentDay');
+
+
+
+
+function displayTime() {
+    var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    currentDayEl. text(currentTime);
+}
