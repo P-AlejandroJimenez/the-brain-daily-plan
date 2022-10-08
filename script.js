0b370a30f5c963b4b239f98f4ec0ae5c9a2ceb14
P-AlejandroJimenez <110042398+P-AlejandroJimenez@users.noreply.github.com>
2022-10-08 12:50:47 -0600

    The moment.js clock was added and made functional atop the page.

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ var currentDayEl = $('#currentDay');
 
 function displayTime() {
     var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    currentDayEl. text(currentTime);
+    currentDayEl.text(currentTime);
 }
+setInterval(displayTime, 1000);
